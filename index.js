af5d48da9fcec4542b9dcded83274911af2b518b
Filipe <me@filipeneves.net>
2019-10-15 10:53:43 +0800

    Adding comments

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+/**
+ * @file BungieNET.js
+ * @description A promise-based Node.js library for the Bungie.net Platform API
+ * @copyright Copyright(c) 2017 Filipe 'Nelen' Neves <me@filipeneves.net>
+ * @license MIT
+ * @version 1.0.0
+ * @module index
+ * @requires src/bungie
+ */
+
+ /**
+  * @typedef {Object} Bungie
+  * @prop {Object} group Group API methods
+  * @prop {Object} users Users API methods
+  * @prop {Function} get Perform a single GET request to Bungie.net API
+  * @prop {Function} post Perform a single POST request to Bungie.net API
+  * @prop {Function} all Perform multiple requests to the Bungie.net API
+  */
 const Bungie = require('./src/bungie')
 
+/**
+ * Initialize the Bungie.net instance
+ * 
+ * @param {String} key API Key from Bungie.net
+ * @param {Object} instance An axios compatible instance configuration
+ */
 const config = function config(key, instance = {}) {
     return new Bungie(key, instance)
 }
